043840b1ebe982545a596ee338cb256ec4ff5564
AndresRIvera32 <andres-illidan92@hotmail.com>
2021-04-28 11:06:21 -0500

    first commit

---
+++ src/main/java/com/springboot/app/commons/users/SpringbootServiceUsersCommonsApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.app.commons.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class SpringbootServiceUsersCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceUsersCommonsApplication.class, args);
+	}
+
+}
